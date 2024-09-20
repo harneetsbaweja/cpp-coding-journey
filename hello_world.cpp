c17843dd4c1c9134306526ff50290630f1c00cf4
harneetsbaweja <harneetbaweja2@gmail.com>
2024-09-21 00:17:18 +0100

    Inital commit

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+  int a;
+  double b;
+  char c;
+  a = 1;
+  b = 0.1;
+  c = 'a';
+  std::cout << "a = " << a << '\n';
+  printf("%d %f %c\n", a, b, c);
+  return 0;
+}
