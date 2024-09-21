c293b97cbd22e7ce6717150bf7f2ad6e8b47fcb8
harneetsbaweja <harneetbaweja2@gmail.com>
2024-09-21 22:27:41 +0100

    Included special case 1 and used bool prime instead of counting factors.

--- prime_numbers.cpp
+++ prime_numbers.cpp
@@ -2,27 +2,35 @@
 
 int main()
 {
-    int number = 24;
-    int factor = 0;
+    int number = 1;
+    bool prime = 1;
+
+    if (number == 1)
+    {
+        std::cout<<number<<" is neither prime nor composite";
+    }
+
+    goto skip_prime_check;
 
     for (int i = 2; i < number; i++)
     {
         if (number % i == 0)
         {
-            factor = i;
+            prime = 0;
             break;
         }
     }
 
-    if (factor == 0)
-    {
-        std::cout<<number<<" is prime";    
-    }
-    
-    if (factor > 0)
+    if (prime == 0)
     {
         std::cout<<number<<" is not prime";
     }
 
+    if (prime == 1)
+    {
+        std::cout<<number<<" is prime";    
+    }
+
+    skip_prime_check:
     return 0;
 }
